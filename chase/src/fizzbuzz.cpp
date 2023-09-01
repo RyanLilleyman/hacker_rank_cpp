4bb0f7fc1adcf7ea98c0b90ae113b03b49730e6b
RyanLilleyman <lilleymanryan2020@gmail.com>
2023-09-01 17:17:59 -0400

    Add fizzBuzz.cpp

--- chase/src/fizzbuzz.cpp
+++ chase/src/fizzbuzz.cpp
@@ -9,7 +9,7 @@ string rtrim(const string &);
  */
 
 void fizzBuzz(int n) {
-  for (int i = 0; i <= n; i++) {
+  for (int i = 1; i <= n; i++) {
     if (i % 5 == 0 && i % 3 == 0) {
       cout << "FizzBuzz"
            << "\n";
